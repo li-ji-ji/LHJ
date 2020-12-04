968d70a1cbd47c98dfcd3a1be800d6cd5dd28547
lhj <1187382451@qq.com>
2020-12-05 02:12:01 +0800

    添加字体配置文件 2020-12-5 02:11 ---lhj

---
+++ manjaro-config/font-config/install.sh
@@ -0,0 +1,7 @@
+sudo pacman -S ttf-roboto noto-fonts ttf-dejavu
+# 文泉驿
+sudo pacman -S wqy-bitmapfont wqy-microhei wqy-microhei-lite wqy-zenhei
+# 思源字体
+sudo pacman -S noto-fonts-cjk adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts
+mkdir ~/.config/fontconfig
+touch ~/.config/fontconfig/fonts.conf
