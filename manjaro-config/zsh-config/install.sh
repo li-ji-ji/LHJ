9d65f4bb957d7b2801d01f1c8c692b22710eb543
lhj <1187382451@qq.com>
2020-12-14 21:42:57 +0800

    添加zsh插件配置 2020-12-14 21:42 ---lhj

---
+++ manjaro-config/zsh-config/install.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
