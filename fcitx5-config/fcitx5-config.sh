1a0caa794ec4f06668678028f2d47eeadc678d96
li-ji-ji <1187382451@qq.com>
2021-02-07 00:35:28 +0800

    fcitx5 config

--- fcitx5-config/fcitx5-config.sh
+++ fcitx5-config/fcitx5-config.sh
@@ -1,2 +1,9 @@
 mkdir -p ~/.local/share/fcitx5/themes/fcitx5-dark-transparent
 git clone https://github.com/hosxy/fcitx5-dark-transparent.git ~/.local/share/fcitx5/themes/fcitx5-dark-transparent
+
+fcitx5-pinyin-zhwiki 肥猫词库
+
+vim ~/.xprofile
+export GTK_IM_MODULE=fcitx5
+export QT_IM_MODULE=fcitx5
+export XMODIFIERS=@im=fcitx
